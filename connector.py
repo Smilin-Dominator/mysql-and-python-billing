a395104931910c5f9e97d7d7c33b91fa8c0d6358
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-04-28 10:23:44 +0530

    Added Functions
    Added the cursor, execution, for loop and all.

--- connector.py
+++ connector.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3.7.9
 import mysql.connector
 
-mydb = mysql.connector.connect(
+mydb = mysql.connector.connect( 
   host="192.168.0.101",
   user="smilin_dominator",
   password="CaptainPrice@2356",
   database='Miscellaneous'
-)
+) # The Credz
+
+id = input("ID: ") # ID As In The First Column
+sql_select_Query = f"select * from paddigurlTest WHERE id = {id}" # This Will Be Sent To The Database
+cursor = mydb.cursor()
+cursor.execute(sql_select_Query)
+records = cursor.fetchall()
+for row in records:
+  print(row[1])
