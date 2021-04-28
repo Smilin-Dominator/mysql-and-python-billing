2f97fce8aa7229dcd920322aa41874b21060a9fe
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-04-28 10:12:04 +0530

    Database Added

---
+++ connector.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.7.9
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="192.168.0.101",
+  user="smilin_dominator",
+  password="CaptainPrice@2356",
+  database='Miscellaneous'
+)
