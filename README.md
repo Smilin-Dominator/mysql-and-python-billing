6c67800b60bd0a7dda003a181e3056bcf66d837d
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-05-22 13:55:57 +0530

    Removed Debugging Code Snippets, Altered README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-mysql-and-python-billing
+# MySQL and Python Billing
+
+### I made this random program to test out if I could extract data from a MySQL Server
+### And Add Them All And Get The Balance. And it succeeded! After 3 Days of trying I gave up on it
+### But then randomly picked it up 3 weeks later, and fixed the bugs!<br>
+
+# How To Use It?
+
+### To Use it, replace my dummy SQL Creds with your SQL Server credentials, the Query and the Array to your need. (connectory.py)
+### price.py and main.py are fine by themselves!
--- price.py
+++ price.py
@@ -6,13 +6,10 @@ logging.basicConfig(filename='log.txt', format=log_format, datefmt='[%Y-%m-%d] [
 temp = open('tmp.txt', 'r') 
 tot = 0
 price_unchained = (temp.read().splitlines())
-print(price_unchained)
 try:
   desired_array = [int(numeric_string) for numeric_string in price_unchained]
   for i in range(0, len(desired_array)):
       tot = tot + desired_array[i]
-      print(i)
-  print(tot)
 except Exception as err2:
   print(err2)
   logging.error(err2)
@@ -21,7 +18,11 @@ print(f'\nTotal:', str(tot))
 logging.info(f'Total: Rs. {tot}')
 cu = int(input('Cash Given: Rs. '))
 logging.info(f'Cash Given: Rs. {cu}')
-bal = int(cu - tot)
+try:
+  bal = int(cu - tot)
+except Exception as e:
+  print(f"Error: {e}")
+  logging.error(e)
 if bal == 0:
   print('\nNo Balance!')
   logging.info('No Balance')
