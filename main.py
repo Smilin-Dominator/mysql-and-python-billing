283b649e882d94944ea6580af7b9a6fcc077adf5
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-05-22 13:42:27 +0530

    Only calls price.py now

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 key = 2
 while key == 2:
     print('Welcome! To The Terminal! ')
-    os.system('connector.py')
     os.system('price.py')
     key = int(input("Would You Like To Stop, Or Continue? (1/2)\n: "))
     if key == 1:
