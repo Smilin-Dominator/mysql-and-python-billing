9a775f6ffcb5dc3d6ae1dfd2faba0c0b62a3b29b
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-05-29 12:50:04 +0530

    Refined

--- connector.py
+++ connector.py
@@ -1,40 +1,42 @@
 import mysql.connector
-import os
 import logging
 
 log_format = '%(asctime)s : %(message)s'
-logcon = logging.basicConfig(filename='log.txt', format=log_format, datefmt='[%Y-%m-%d] [%H:%M:%S]', level=logging.DEBUG)
+logcon = logging.basicConfig(filename='log.txt', format=log_format, datefmt='[%Y-%m-%d] [%H:%M:%S]',
+                             level=logging.DEBUG)
 
-tmp = open('tmp.txt', 'w+') #Temporary File To Write The Amounts
+tmp = open('tmp.txt', 'w+')  # Temporary File To Write The Amounts
 
 mydb = mysql.connector.connect(
-  auth_plugin='mysql_native_password',
-  host="192.168.10.5",
-  user="smilin_dominator",
-  password="Barney2356",
-  database='miscellaneous'
-) # The Credentials For The Database
+    auth_plugin='mysql_native_password',
+    host="192.168.10.5",
+    user="smilin_dominator",
+    password="Barney2356",
+    database='miscellaneous'
+)  # connection time
 
-while id != 'Stop':
-  try:
-    id = input("\nID: ") # ID As In The First Column
-    if id == 'stop':
-      tmp.flush()
-      break
-    else:
-      sql_select_Query = f"select * from paddigurlTest WHERE id = {id}" # This Will Be Sent To The Database
-      cursor = mydb.cursor() # This Is As If You Were Entering It Yourself
-      cursor.execute(sql_select_Query) # Executes
-      records = cursor.fetchall() # Gets All The Outputs
-      for row in records: 
-        name = row[1]
-        price = row[2]
-        quantity = int(input("Quantity: "))
-        print(f"\nName  : {name}")
-        print(f"Price : {price}")
-        tmp.writelines(f'{int(price) * quantity}\n')
-        logging.info(f'Sold {quantity} Of Item;\n{records}, totalling Rs. {str(int(price) * quantity)}')
-      tmp.flush()
-  except Exception as rim:
-    print('An Error Occured!\n\n', rim)
-    logging.error("MySQL Error:\n\n",rim)
+id = 69
+while id != ' ':
+    try:
+        id = input("\nID: ")  # ID As In The First Column
+        if id == '':
+            tmp.flush()
+            break
+        else:
+            sql_select_Query = f"select * from paddigurlTest WHERE id = {id}"  # This Will Be Sent To The Database
+            cursor = mydb.cursor()  # This Is As If You Were Entering It Yourself
+            cursor.execute(sql_select_Query)  # Executes
+            records = cursor.fetchall()  # Gets All The Outputs
+            for row in records:
+                name = row[1]
+                price = row[2]
+                print(f"\nName  : {name}")
+                print(f"Price : {price}")
+                quantity = int(input("\nQuantity: "))
+                tmp.writelines(f'{int(price) * quantity}\n')
+                logging.info(
+                    f'Sold {quantity} Of Item;\n{records}, bringing the total to Rs. {str(int(price) * quantity)}')
+            tmp.flush()
+    except Exception as rim:
+        print('An Error Occured!\n\n', rim)
+        logging.error("Error:\n\n", rim)
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 key = 2
 while key == 2:
     print('Welcome! To The Terminal! ')
+    check = os.path.exists('bills/')
+    if check == False:
+        os.mkdir("bills/")
     os.system('price.py')
     key = int(input("Would You Like To Stop, Or Continue? (1/2)\n: "))
     if key == 1:
