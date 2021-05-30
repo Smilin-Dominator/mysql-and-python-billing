d7e71a8db7728381608eb2a31a3bdb403f92dfde
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-05-30 12:58:18 +0530

    Comments

--- connector.py
+++ connector.py
@@ -3,7 +3,7 @@ import logging
 import time
 import os
 
-log_format = '%(asctime)s : %(message)s'
+log_format = '%(asctime)s : %(message)s'  # this basically says that the time and date come first, error next
 logging.basicConfig(filename='log.txt', format=log_format, datefmt='[%Y-%m-%d] [%H:%M:%S]', level=logging.DEBUG)
 
 mydb = mysql.connector.connect(
