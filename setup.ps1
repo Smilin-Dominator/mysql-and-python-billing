983130fe69009d5dcca890b2269242bdda31efcd
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-07-06 23:58:16 -0500

    Made The Shell Scripts Install Everything In requirements.txt

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
-Write-Host "Installing Dependencies...."
-pip3 install mysql-connector-python pandas
-Write-Host "Success!"
+Write-Host "[*] Installing Dependencies...."
+pip3 install -r requirements.txt
+Write-Host "[*] Success!"
 New-Item -Name "log.txt" -ItemType "file"
-Write-Host "Finished.. Restart (main.py).."
+Write-Host "[*] Finished.. Restart (main.py).."
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-echo "Installing Dependencies.."
-pip3 install mysql-connector-python pandas
-echo "Success!"
+echo "[*] Installing Dependencies.."
+pip3 install -r requirements.txt
+echo "[*] Success!"
 touch log.txt
-echo "Successful.. Restart (main.py)..."
+echo "[*] Successful.. Restart (main.py)..."
