f160f65ccf985350bbad7acd5e47289d137aecd5
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-09-11 12:18:06 +0530

    Colours and Commands
    Signed-off-by: Devisha Padmaperuma <devisha.padmaperuma@gmail.com>

--- sql_client.py
+++ sql_client.py
@@ -2,7 +2,7 @@ import getpass
 import pandas as pd
 import logging
 import hashlib
-from configuration import variables
+from configuration import variables, colours
 
 logging.basicConfig(filename='log.txt', format=variables.log_format, datefmt='[%Y-%m-%d] [%H:%M:%S]',
                     level=logging.DEBUG)
@@ -26,15 +26,22 @@ def auth(mydb):
             badPass = True
 
 
-help_string = "\nhelp --> displays this\nshow all --> selects all the dolls\nbye --> exits\nadd --> adds an " \
-              "item\nremove --> removes an item\nchange --> alters an item\nadd id --> adds item with ID\n custom -->" \
-              "executes your custom query\n"
+help_string = """
+    help            -> displays this
+    show all        -> shows all dolls
+    show specific   -> allows you to set one conditions
+    show advanced   -> allows you to set multiple conditions
+    bye             -> quit
+"""
 
 theformat = "{:<2}{:^40}{:>5}"
 
 
 class commands:
-    select = "SELECT * FROM %s WHERE %s = %s;"
+    select_equals = "SELECT * FROM %s WHERE %s = %s;"
+    select_greater = "SELECT * FROM %s WHERE %s > %s;"
+    select_lesser = "SELECT * FROM %s WHERE %s < %s;"
+    select_not_equals = "SELECT * FROM %s WHERE %s != %s;"
     select_all = "SELECT * FROM %s;"
     insert = "INSERT INTO %s(%s) VALUES(%s);"
     update = "UPDATE %s SET %s = %s;"
@@ -42,14 +49,19 @@ class commands:
 
 
 def print_items(items):
-    print(theformat.format("ID", "Name", "Price"))
+    print(theformat.format(f"{colours.LightRed}ID{colours.ENDC}", f"{colours.LightMagenta}Name{colours.ENDC}", f"{colours.LightGray}Price{colours.ENDC}"))
     for id, name, price in items:
+        id = f"{colours.Red}{id}{colours.ENDC}"
+        name = f"{colours.Yellow}{name}{colours.ENDC}"
+        price = f"{colours.Green}{price}{colours.ENDC}"
         print(theformat.format(id, name, price))
 
 
-def single_condition_get_items(cursor, table: str, condition: str = None, value: str = None):
+def get_items(cursor, table: str, condition: str = None, value: str = None, query: str = None):
     if (condition is not None) and (value is not None):
         cursor.execute(commands.select % (table, condition, value))
+    elif query is not None:
+        cursor.execute(query)
     else:
         cursor.execute(commands.select_all % table)
     return cursor.fetchall()
@@ -62,7 +74,9 @@ def main(mydb):
         if command == "help":
             print(help_string)
         elif command == "show all":
-            rows = single_condition_get_items(mycursor, "paddigurlTest")
+            rows = get_items(cursor=mycursor, table="paddigurlTest")
             print_items(rows)
+        elif command == "show specific":
+            print()
         elif command == "bye":
             break
