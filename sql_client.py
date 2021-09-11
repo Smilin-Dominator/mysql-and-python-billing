895e167edda3e9a6ddc1faebcffc4494e4909d42
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-09-11 14:18:10 +0530

    Modified Help String, And Added Comments
    Signed-off-by: Devisha Padmaperuma <devisha.padmaperuma@gmail.com>

--- sql_client.py
+++ sql_client.py
@@ -26,12 +26,23 @@ def auth(mydb):
 
 
 help_string = """
-    help            -> displays this
-    show all        -> shows all dolls
-    show specific   -> allows you to set one condition
-    show advanced   -> allows you to set multiple conditions
-    show custom     -> write your own query (for the paddigurlTest table only)
-    bye             -> quit
+    Getting Data:
+        show all        -> shows all dolls
+        show specific   -> allows you to set one condition
+        show advanced   -> allows you to set multiple conditions
+        show custom     -> write your own query (for the paddigurlTest table only)
+        
+    Inserting Data:
+        add             -> adds an item, prompts for Name and Price
+        add id          -> adds an item, prompts for ID, Name and Price
+        
+    Modifying Data:
+        update          -> Update the name and price of an item
+        delete          -> removes an item from paddigurlTest and adds it to paddigurlRemoved
+    
+    Miscellaneous:
+        help            -> displays this
+        bye             -> quit
 """
 
 theformat = "{:<2}{:^40}{:>5}"
@@ -52,7 +63,8 @@ class commands:
 
 def print_items(items):
     print("\n")
-    print(theformat.format(f"{colours.LightRed}ID{colours.ENDC}", f"{colours.LightMagenta}Name{colours.ENDC}", f"{colours.LightGray}Price{colours.ENDC}"))
+    print(theformat.format(f"{colours.LightRed}ID{colours.ENDC}", f"{colours.LightMagenta}Name{colours.ENDC}",
+                           f"{colours.LightGray}Price{colours.ENDC}"))
     for id, name, price in items:
         id = f"{colours.Red}{id}{colours.ENDC}"
         name = f"{colours.Yellow}{name}{colours.ENDC}"
@@ -60,7 +72,17 @@ def print_items(items):
         print(theformat.format(id, name, price))
 
 
-def get_items(cursor, table: str = None, operation: str = None, condition: str = None, value: str = None, query: str = None):
+"""
+Get Items. This is the single most Jam-Packed function in this file.
+
+If the Condition, Value and Operation are present, it'll execute execute the single condition mode (show specific)
+If the query is present it'll execute either (show specific) or (show custom)
+If neither are there, it'll execute (show all)
+"""
+
+
+def get_items(cursor, table: str = None, operation: str = None, condition: str = None, value: str = None,
+              query: str = None):
     if (condition is not None) and (value is not None) and (operation is not None):
         if operation == "=":
             cursor.execute(commands.select_equals % (table, condition, value))
@@ -87,8 +109,14 @@ def main(mydb):
     mycursor = mydb.cursor()
     while True:
         command = input(f"\n{colours.LightRed}Smilin_DB> {colours.ENDC}")
+
+        # -------- Miscellaneous -----------#
         if command == "help":
             print(help_string)
+        elif command == "bye":
+            break
+
+        # ---------- Get Data ----------------#
         elif command == "show all":
             rows = get_items(cursor=mycursor, table="paddigurlTest")
             print_items(rows)
@@ -96,14 +124,17 @@ def main(mydb):
             field = input(f"{colours.Cyan}[*] Field: {colours.ENDC}")
             operator = input(f"{colours.Red}[*] Operator (>, =, <, !=, <=, >=): {colours.ENDC}")
             value = input(f"{colours.Yellow}[*] Value: {colours.ENDC}")
-            print_items(get_items(cursor=mycursor, table="paddigurlTest", operation=operator, condition=field, value=value))
+            print_items(
+                get_items(cursor=mycursor, table="paddigurlTest", operation=operator, condition=field, value=value)
+            )
         elif command == "show advanced":
             query = "SELECT * FROM paddigurlTest WHERE "
-            print(f"{colours.LightGreen}[*] Enter The Fields And Their Values (When Done Select ';' as Seperator){colours.ENDC}")
+            print(
+                f"{colours.LightGreen}[*] Enter The Fields And Their Values (When Done Select ';' as Seperator){colours.ENDC}")
             i = 0
             while True:
                 try:
-                    field = input(f"{colours.Cyan}[*] Field {i+1}: {colours.ENDC}")
+                    field = input(f"{colours.Cyan}[*] Field {i + 1}: {colours.ENDC}")
                     operator = input(f"{colours.Red}[*] Operator (>, =, <, !=, <=, >=): {colours.ENDC}")
                     value = input(f"{colours.Yellow}[*] Value: {colours.ENDC}")
                     seperator = input(f"{colours.White}[*] Seperator (NOT, AND, OR, ;): {colours.ENDC}")
@@ -119,5 +150,15 @@ def main(mydb):
         elif command == "show custom":
             query = input(f"{colours.Cyan}[*] Query: {colours.ENDC}")
             print_items(get_items(cursor=mycursor, query=query))
-        elif command == "bye":
-            break
+
+        # -------------- Insert Data --------------------#
+        elif command == "add":
+            pass
+        elif command == "add id":
+            pass
+
+        # -------------- Modifying Data -------------------#
+        elif command == "update":
+            pass
+        elif command == "delete":
+            pass
