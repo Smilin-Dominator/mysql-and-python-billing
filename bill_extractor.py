f16e915ef85e3f1f79e06b01bf1d10787891c36d
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-10-21 22:46:03 +0530

    Changed name algorithm from strip to char count Since .strip() would screw it up a little (lot)
    Signed-off-by: Devisha Padmaperuma <devisha.padmaperuma@gmail.com>

--- bill_extractor.py
+++ bill_extractor.py
@@ -1,11 +1,22 @@
-import io
-
-
 class bill(object):
 
     def __init__(self, stream: io.TextIOWrapper):
         self.file = stream
         self.lines = self.file.read().splitlines()
+
+    def date(self) -> str:
+        # **Date: <span style="color:blue">21/10/2021</span>**<br>
+        pass
+
+    def time(self) -> str:
+        # **Time: <span style="color:red">09.42 PM</span>**<br>
+        pass
+
+    def customer(self) -> str:
+        # **Customer: <span style="color:green">Barney Stinson</span>**<br>
+        name = self.lines[6]
+        name = name[:-13]
+        return name[38:]
 
     def grand_total(self) -> str:
         grand_total = " ".join([e for e in self.lines if e.startswith('**Grand Total:')])
