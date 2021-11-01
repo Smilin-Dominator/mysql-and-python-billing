0d4fc74508ea6d6b1b1e75ec1256cd2d8ed4ce7f
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-11-01 09:22:47 +0530

    Added 'match-case' to sql_client

--- sql_client.py
+++ sql_client.py
@@ -8,6 +8,7 @@ logging.basicConfig(filename='log.txt', format=variables.log_format, datefmt='[%
 
 
 def auth(mydb):
+    count = 0
     badPass = True
     while badPass:
         passwd = input("Master Password", override="red", password=True)
@@ -20,7 +21,10 @@ def auth(mydb):
             main(mydb)
             break
         else:
+            count += 1
             logging.warning("Wrong Pass")
+            if count >= 3:
+                break
             badPass = True
 
 
@@ -83,20 +87,21 @@ If neither are there, it'll execute (show all)
 def get_items(cursor, table: str = None, operation: str = None, condition: str = None, value: str = None,
               query: str = None):
     if (condition is not None) and (value is not None) and (operation is not None):
-        if operation == "=":
-            cursor.execute(commands.select_equals % (table, condition, value))
-        elif operation == ">":
-            cursor.execute(commands.select_greater % (table, condition, value))
-        elif operation == ">=":
-            cursor.execute(commands.select_greatere % (table, condition, value))
-        elif operation == "<":
-            cursor.execute(commands.select_lesser % (table, condition, value))
-        elif operation == "<=":
-            cursor.execute(commands.select_lessere % (table, condition, value))
-        elif operation == "!=":
-            cursor.execute(commands.select_not_equals % (table, condition, value))
-        else:
-            print("INVALID OPERATOR")
+        match operation:
+            case "=":
+                cursor.execute(commands.select_equals % (table, condition, value))
+            case ">":
+                cursor.execute(commands.select_greater % (table, condition, value))
+            case ">=":
+                cursor.execute(commands.select_greatere % (table, condition, value))
+            case "<":
+                cursor.execute(commands.select_lesser % (table, condition, value))
+            case "<=":
+                cursor.execute(commands.select_lessere % (table, condition, value))
+            case "!=":
+                cursor.execute(commands.select_not_equals % (table, condition, value))
+            case _:
+                print("INVALID OPERATOR")
     elif query is not None:
         cursor.execute(query)
     else:
@@ -107,114 +112,115 @@ def get_items(cursor, table: str = None, operation: str = None, condition: str =
 def main(mydb):
     mycursor = mydb.cursor()
     while True:
-        command = input(f"\n[deep_pink4]Smilin_DB> [/deep_pink4]")
+        command = input(f"\nSmilin_DB>", override="deep_pink4")
 
-        # -------- Miscellaneous -----------#
-        if command == "help":
-            print(help_string)
-        elif command == "bye":
-            break
+        match command:
+            # -------- Miscellaneous -----------#
+            case "help":
+                print(help_string)
+            case "bye":
+                break
 
-        # ---------- Get Data ----------------#
-        elif command == "show all":
-            rows = get_items(cursor=mycursor, table="paddigurlTest")
-            print_items(rows)
-        elif command == "show specific":
-            field = input(f"[*] Field", override="cyan")
-            operator = input(f"[*] Operator (>, =, <, !=, <=, >=)", override="red")
-            value = input(f"[*] Value", override="yellow")
-            print_items(
-                get_items(cursor=mycursor, table="paddigurlTest", operation=operator, condition=field, value=value)
-            )
-        elif command == "show advanced":
-            query = "SELECT * FROM paddigurlTest WHERE "
-            info("Enter The Fields And Their Values (When Done Select ';' as Seperator)", override="green_yellow")
-            i = 0
-            while True:
-                try:
-                    field = input(f"[*] Field {i + 1}", override="cyan")
-                    operator = input(f"[*] Operator (>, =, <, !=, <=, >=)", override="red")
-                    value = input(f"[*] Value", override="cyan")
-                    separator = input(f"[*] Seperator (NOT, AND, OR, ;)", override="white")
-                    i += 1
-                    if separator == ";":
-                        query += f"`{field}` {operator} '{value}';"
+            # ---------- Get Data ----------------#
+            case "show all":
+                rows = get_items(cursor=mycursor, table="paddigurlTest")
+                print_items(rows)
+            case "show specific":
+                field = input(f"[*] Field", override="cyan")
+                operator = input(f"[*] Operator (>, =, <, !=, <=, >=)", override="red")
+                value = input(f"[*] Value", override="yellow")
+                print_items(
+                    get_items(cursor=mycursor, table="paddigurlTest", operation=operator, condition=field, value=value)
+                )
+            case "show advanced":
+                query = "SELECT * FROM paddigurlTest WHERE "
+                info("Enter The Fields And Their Values (When Done Select ';' as Seperator)", override="green_yellow")
+                i = 0
+                while True:
+                    try:
+                        field = input(f"[*] Field {i + 1}", override="cyan")
+                        operator = input(f"[*] Operator (>, =, <, !=, <=, >=)", override="red")
+                        value = input(f"[*] Value", override="cyan")
+                        separator = input(f"[*] Seperator (NOT, AND, OR, ;)", override="white")
+                        i += 1
+                        if separator == ";":
+                            query += f"`{field}` {operator} '{value}';"
+                            break
+                        else:
+                            query += f"`{field}` {operator} '{value}' {separator} "
+                    except KeyboardInterrupt:
                         break
-                    else:
-                        query += f"`{field}` {operator} '{value}' {separator} "
-                except KeyboardInterrupt:
-                    break
-            print_items(get_items(cursor=mycursor, query=query))
-        elif command == "show custom":
-            query = input(f"[*] Query: ", override="cyan")
-            print_items(get_items(cursor=mycursor, query=query))
+                print_items(get_items(cursor=mycursor, query=query))
+            case "show custom":
+                query = input(f"[*] Query: ", override="cyan")
+                print_items(get_items(cursor=mycursor, query=query))
 
-        # -------------- Insert Data --------------------#
-        elif command == "add":
-            name = input(f"[*] Name", override="green")
-            price = int(input(f"[*] Price", override="yellow"))
-            mycursor.execute(commands.insert % ("paddigurlTest", "name, price", f"'{name}', {price}"))
-            mydb.commit()
-            print(f"[!] Success! Inserted {mycursor.rowcount} Row(s)!", override="spring_green2")
-        elif command == "add id":
-            ID = int(input(f"[*] ID", override="magenta"))
-            name = input(f"[*] Name", override="misty_rose3")
-            price = int(input(f"[*] Price", override="yellow"))
-            mycursor.execute(commands.insert % ("paddigurlTest", "id, name, price", f"{ID}, '{name}', {price}"))
-            mydb.commit()
-            print(f"[!] Success! Inserted {mycursor.rowcount} Row(s)!", override="bright_green")
-        elif command == "add multiple":
-            values = ""
-            print(f"[*] Enter The Values Below (Ctrl+C when Done)", override="white")
-            while True:
-                try:
-                    name = input(f"[*] Name", override="pale_turquoise4")
-                    price = int(input(f"[*] Price", override="steel_blue"))
-                    values += f"('{name}', {price}),"
-                except KeyboardInterrupt:
-                    values = values[:-1]
-                    values += ";"
-                    break
-            mycursor.execute(commands.insert_multiple % ("paddigurlTest", "name, price", f"{values}"))
-            mydb.commit()
-            print(f"[!] Success! Inserted {mycursor.rowcount} Row(s)!", override="green")
+            # -------------- Insert Data --------------------#
+            case "add":
+                name = input(f"[*] Name", override="green")
+                price = int(input(f"[*] Price", override="yellow"))
+                mycursor.execute(commands.insert % ("paddigurlTest", "name, price", f"'{name}', {price}"))
+                mydb.commit()
+                print(f"[!] Success! Inserted {mycursor.rowcount} Row(s)!", override="spring_green2")
+            case "add id":
+                ID = int(input(f"[*] ID", override="magenta"))
+                name = input(f"[*] Name", override="misty_rose3")
+                price = int(input(f"[*] Price", override="yellow"))
+                mycursor.execute(commands.insert % ("paddigurlTest", "id, name, price", f"{ID}, '{name}', {price}"))
+                mydb.commit()
+                print(f"[!] Success! Inserted {mycursor.rowcount} Row(s)!", override="bright_green")
+            case "add multiple":
+                values = ""
+                print(f"[*] Enter The Values Below (Ctrl+C when Done)", override="white")
+                while True:
+                    try:
+                        name = input(f"[*] Name", override="pale_turquoise4")
+                        price = int(input(f"[*] Price", override="steel_blue"))
+                        values += f"('{name}', {price}),"
+                    except KeyboardInterrupt:
+                        values = values[:-1]
+                        values += ";"
+                        break
+                mycursor.execute(commands.insert_multiple % ("paddigurlTest", "name, price", f"{values}"))
+                mydb.commit()
+                print(f"[!] Success! Inserted {mycursor.rowcount} Row(s)!", override="green")
 
-        # -------------- Modifying Data -------------------#
-        elif command == "update":
-            ID = int(input(f"[*] ID", override="green"))
-            matches = get_items(cursor=mycursor, query="SELECT * FROM paddigurlTest WHERE ID = %d;" % ID)
-            if not matches:
-                print(f"[!] No Matches!", override="red")
-                break
-            else:
-                try:
-                    print_items(matches)
-                    print(f"\n(Ctrl+C To Abort)\n", override="red")
-                    new_name = input(f"[*] New Name", override="green")
-                    new_price = int(input(f"[*] New Price", override="yellow"))
-                    logging.warning("Changing ID: %d\nName: %s => %s\nPrice: %d => %d" % (ID, matches[0][1], new_name, matches[0][2], new_price))
-                    mycursor.execute("UPDATE paddigurlTest SET name = '%s', price = %d WHERE ID = %d;" % (new_name, new_price, ID))
-                    mydb.commit()
-                    print(f"[*] Success!", override="green")
-                except KeyboardInterrupt:
+            # -------------- Modifying Data -------------------#
+            case "update":
+                ID = int(input(f"[*] ID", override="green"))
+                matches = get_items(cursor=mycursor, query="SELECT * FROM paddigurlTest WHERE ID = %d;" % ID)
+                if not matches:
+                    print(f"[!] No Matches!", override="red")
                     break
-        elif command == "delete":
-            ID = int(input(f"[*] ID", override="green"))
-            matches = get_items(cursor=mycursor, query="SELECT * FROM paddigurlTest WHERE ID = %d;" % ID)
-            if not matches:
-                print(f"[!] No Matches!", override="red")
-                break
-            else:
-                try:
-                    print_items(matches)
-                    go = input("\n[#] Proceed? (y/n)", override="yellow")
-                    if go == "y":
-                        mycursor.execute("DELETE FROM paddigurlTest WHERE id = %d;" % ID)
-                        logging.warning("Removed ID: %d\nName: %s\nPrice: %d" % (ID, matches[0][1], matches[0][2]))
-                        mycursor.execute("INSERT INTO paddigurlRemoved(id, name, price) VALUES(%d, '%s', %d);" % (ID, matches[0][1], matches[0][2]))
+                else:
+                    try:
+                        print_items(matches)
+                        print(f"\n(Ctrl+C To Abort)\n", override="red")
+                        new_name = input(f"[*] New Name", override="green")
+                        new_price = int(input(f"[*] New Price", override="yellow"))
+                        logging.warning("Changing ID: %d\nName: %s => %s\nPrice: %d => %d" % (ID, matches[0][1], new_name, matches[0][2], new_price))
+                        mycursor.execute("UPDATE paddigurlTest SET name = '%s', price = %d WHERE ID = %d;" % (new_name, new_price, ID))
                         mydb.commit()
                         print(f"[*] Success!", override="green")
-                    else:
+                    except KeyboardInterrupt:
                         break
-                except KeyboardInterrupt:
+            case "delete":
+                ID = int(input(f"[*] ID", override="green"))
+                matches = get_items(cursor=mycursor, query="SELECT * FROM paddigurlTest WHERE ID = %d;" % ID)
+                if not matches:
+                    print(f"[!] No Matches!", override="red")
                     break
+                else:
+                    try:
+                        print_items(matches)
+                        go = input("\n[#] Proceed? (y/n)", override="yellow")
+                        if go == "y":
+                            mycursor.execute("DELETE FROM paddigurlTest WHERE id = %d;" % ID)
+                            logging.warning("Removed ID: %d\nName: %s\nPrice: %d" % (ID, matches[0][1], matches[0][2]))
+                            mycursor.execute("INSERT INTO paddigurlRemoved(id, name, price) VALUES(%d, '%s', %d);" % (ID, matches[0][1], matches[0][2]))
+                            mydb.commit()
+                            print(f"[*] Success!", override="green")
+                        else:
+                            break
+                    except KeyboardInterrupt:
+                        break
