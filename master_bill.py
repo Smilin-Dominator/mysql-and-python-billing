d10e65683adcbfafb74c1bee4ff1452934b4424d
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-11-24 12:02:21 +0530

    Absolute imports in master-bill

--- master_bill.py
+++ master_bill.py
@@ -1,7 +1,7 @@
 import logging
-import os
-import time
-import glob
+from os import listdir, remove, path
+from time import strftime
+from glob import glob
 from pathlib import Path
 from configuration import print, input, console
 from rich.table import Table
@@ -20,14 +20,14 @@ master_bill_header = "{:^50}"
 
 class master_bill(object):
 
-    def __init__(self, var_path: str =None, bill=None, main_ar=None):
+    def __init__(self, var_path: str = None, bill=None, main_ar=None):
         self.var_path = var_path
         self.bill = bill
         self.main_ar = main_ar
         self.day = self.var_path[8:].replace("_", " ")
 
     def bill_write(self) -> None:
-        master_bill_path = os.path.join(self.var_path + '/master_bill.txt')
+        master_bill_path = path.join(self.var_path + '/master_bill.txt')
         bill_prep = my_format.format("Name", "Grand Total (Rs.)")
         master_bill_file = open(master_bill_path, 'w+')
         master_bill_file.write(f"{master_bill_header.format(50 * '-')}")
@@ -35,8 +35,8 @@ class master_bill(object):
         master_bill_file.write(f"\n{master_bill_header.format(50 * '-')}")
         master_bill_file.write(f"\n{master_bill_header.format('Daily Sales Report')}")
         master_bill_file.write(f"\n{master_bill_header.format(50 * '-')}")
-        master_bill_file.write(f'\n\nDate: {str(time.strftime("%d/%m/%Y"))}')
-        master_bill_file.write(f"\nTime: {str(time.strftime('%I.%M %p'))}")
+        master_bill_file.write(f'\n\nDate: {str(strftime("%d/%m/%Y"))}')
+        master_bill_file.write(f"\nTime: {str(strftime('%I.%M %p'))}")
         master_bill_file.write(f'\n\n{bill_prep}')
 
         tab = Table(title=f"Master Bill Of {self.day}")
@@ -62,7 +62,7 @@ class master_bill(object):
         console.print(tab)
 
     def bill_collect(self):
-        temp_path = os.path.join(self.var_path + '/' + self.bill)
+        temp_path = path.join(self.var_path + '/' + self.bill)
         temp_file = open(temp_path, 'r')
         temp_read = temp_file.read().splitlines()
 
@@ -77,11 +77,11 @@ class master_bill(object):
 
 
 def delete_previous(start: str):
-    hits = glob.glob(f"./sales_reports/sales-report_from_{start}*")
+    hits = glob(f"./sales_reports/sales-report_from_{start}*")
     logging.info(f"Found file(s) with start date \"{hits}\": {hits}")
     for file in hits:
         logging.warning(f"Removing file: {file}")
-        os.remove(file)
+        remove(file)
 
 
 def sales_reports(multiverse: list):
@@ -109,8 +109,8 @@ def sales_reports(multiverse: list):
         sales_report.write(f"\n{master_bill_header.format(50 * '-')}")
         sales_report.write(f"\n{master_bill_header.format('Weekly Sales Report')}")
         sales_report.write(f"\n{master_bill_header.format(50 * '-')}")
-        sales_report.write(f'\n\nDate: {str(time.strftime("%d/%m/%Y"))}')
-        sales_report.write(f"\nTime: {str(time.strftime('%I.%M %p'))}")
+        sales_report.write(f'\n\nDate: {str(strftime("%d/%m/%Y"))}')
+        sales_report.write(f"\nTime: {str(strftime('%I.%M %p'))}")
         sales_report.write(f'\n\n{my_format.format("Day", "Total Sales")}')
         grandest_total = 0
         for j in range(len(total_ar)):
@@ -124,13 +124,13 @@ def sales_reports(multiverse: list):
 
 
 def main():
-    multiverse = os.listdir('./bills')
+    multiverse = listdir('./bills')
     main_ar = []
     key = input_screen()
     if key == 'T':
-        var_time = time.strftime("%d_of_%B")
+        var_time = strftime("%d_of_%B")
         var_path = f'./bills/{var_time}/'
-        ls_l = os.listdir(var_path)
+        ls_l = listdir(var_path)
         for bill in ls_l:
             if bill.startswith("[BILL]"):
                 to_write = master_bill(var_path=var_path, bill=bill).bill_collect()
@@ -139,8 +139,8 @@ def main():
         sales_reports(multiverse)
     else:
         for directory in multiverse:
-            var_path = os.path.join('./bills/', directory)
-            ls_l = os.listdir(var_path)
+            var_path = path.join('./bills/', directory)
+            ls_l = listdir(var_path)
             for bill in ls_l:
                 if bill.startswith("[BILL]"):
                     to_write = master_bill(var_path=var_path, bill=bill).bill_collect()
