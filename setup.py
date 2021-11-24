671cbc538fc49430a543c7eed281510dbde31113
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-11-24 16:19:37 +0530

    Absolute imports in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-import subprocess
-import time
-import os
-import base64
+from subprocess import run, DEVNULL, SubprocessError
+from time import sleep
+from os import path
+from base64 import b64encode
 from configuration import commands, variables, console, print, input, warning, error
 
 
@@ -9,16 +9,16 @@ def main():
     print("[bold green]Welcome to my Program! Setting Up Config File")
     commands().write_conifguration_file()
     with console.status("[bold green]Initializing..", spinner='dots12') as _:
-        _ if subprocess.run("python -m pip install -r requirements.txt", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True).returncode == 0 else error(msg="While Installing Pip Packages")
+        _ if run("python -m pip install -r requirements.txt", stdout=DEVNULL, stderr=DEVNULL, shell=True).returncode == 0 else error(msg="While Installing Pip Packages")
         console.log("Environment Setup Complete")
-        time.sleep(3)
+        sleep(3)
         console.log("Created Log.txt")
-        time.sleep(3)
+        sleep(3)
         console.log("All Tasks Successful!")
 
 
 def sql(logging, rsa):
-    check_for_file = os.path.exists('./credentials/mysql.txt')
+    check_for_file = path.exists('./credentials/mysql.txt')
     if not check_for_file:
         warning("No MySQL Configuration File Detected, Enter The Details Below.", "bold yellow")
         create_container = input("Would you like to create a docker container? (y/n)", "italic green")
@@ -35,9 +35,9 @@ def sql(logging, rsa):
                     dc = variables.docker_compose % (user, password)
                     docker.write(dc)
                     docker.close()
-                subprocess.run("docker-compose up -d", shell=True)
-                time.sleep(10)
-            except subprocess.SubprocessError:
+                run("docker-compose up -d", shell=True)
+                sleep(10)
+            except SubprocessError:
                 error("An Error Occured (Is Docker-Compose Installed?)")
         else:
             host = input("Host", "bold red")
@@ -47,30 +47,30 @@ def sql(logging, rsa):
             db = input("Database", "bold red")
         with console.status("[bold green]Registering Credentials..", spinner='dots11') as _:
             pubKey, privKey = rsa.newkeys(1096)
-            time.sleep(2)
+            sleep(2)
             console.log("Generated Keys....")
             with open('./credentials/public.pem', 'w') as pop:
                 pubStr = pubKey.save_pkcs1()
                 pop.write(pubStr.decode('utf-8'))
-                encoded = base64.b64encode(pubStr)
+                encoded = b64encode(pubStr)
                 logging.info(f"Binary_Data:{encoded}")
                 pop.close()
-            time.sleep(2)
+            sleep(2)
             console.log("Wrote Public Key..")
             with open('./credentials/private.pem', 'w') as pop:
                 privStr = privKey.save_pkcs1()
                 pop.write(privStr.decode('utf-8'))
-                encoded = base64.b64encode(privStr)
+                encoded = b64encode(privStr)
                 logging.info(f"Binary-Data:{encoded}")
                 pop.close()
-            time.sleep(2)
+            sleep(2)
             console.log("Wrote Private Key..")
             logging.info("Wrote RSA Keys")
             st = f"{host},{user},{port},{password},{db}".encode()
             with open('./credentials/mysql.txt', 'wb+') as mcdonalds:
                 var = rsa.encrypt(st, pubKey)
                 mcdonalds.write(var)
-            time.sleep(2)
+            sleep(2)
             console.log("Successfully Encrypted Credentials")
         if create_container == 'n':
             conf = input("Create Tables? (y/n)", "blue")
